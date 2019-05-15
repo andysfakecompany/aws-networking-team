b0a785320f19ab60c18182b6dcf3eae77d11f4cc
Andy James <ajames@hashicorp.com>
2019-05-15 18:58:03 -0500

    adding outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "fakecompany_vpc_id" {
   value = "${aws_vpc.andys-fake-company.id}"
 }
+
+output "fakecompany_subnet_1" {
+  value = "${aws_subnet.ip_range_one.id}"
+}
+
+output "fakecompany_subnet_2" {
+  value = "${aws_subnet.ip_range_two.id}"
+}
+
+output "fakecompany_subnet_3" {
+  value = "${aws_subnet.ip_range_three.id}"
+}
+
+output "fakecompany_subnet_4" {
+  value = "${aws_subnet.ip_range_four.id}"
+}
